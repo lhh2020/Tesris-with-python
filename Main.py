5a9b4cf2202ca7e2bf8f0df2cff3ad48edcd5bdd
lhh2020 <54210925+lhh2020@users.noreply.github.com>
2022-04-08 15:50:24 +0900

    save

--- Main.py
+++ Main.py
@@ -1,31 +1,8 @@
+import random
+import time
 class Main:
+    block_kind = 7
     block = [
-        [
-            [
-                [0,0,0,0],
-                [0,0,0,0],
-                [0,0,0,0],
-                [0,0,0,0]
-            ],
-            [
-                [0,0,0,0],
-                [0,0,0,0],
-                [0,0,0,0],
-                [0,0,0,0]
-            ],
-            [
-                [0,0,0,0],
-                [0,0,0,0],
-                [0,0,0,0],
-                [0,0,0,0]
-            ],
-            [
-                [0,0,0,0],
-                [0,0,0,0],
-                [0,0,0,0],
-                [0,0,0,0]
-            ]
-        ],
         (
             (
                 (0,0,1,0),
@@ -51,21 +28,235 @@ class Main:
                 (0,0,0,0),
                 (0,0,0,0)
             )
+        ),
+        (
+            (
+                (1,1,0,0),
+                (0,1,1,0),
+                (0,0,0,0),
+                (0,0,0,0)
+            ),
+            (
+                (0,0,1,0),
+                (0,1,1,0),
+                (0,1,0,0),
+                (0,0,0,0)
+            ),
+            (
+                (0,0,0,0),
+                (1,1,0,0),
+                (0,1,1,0),
+                (0,0,0,0)
+            ),
+            (
+                (0,1,0,0),
+                (1,1,0,0),
+                (1,0,0,0),
+                (0,0,0,0)
+            )
+        ),
+        (
+            (
+                (0,1,1,0),
+                (1,1,0,0),
+                (0,0,0,0),
+                (0,0,0,0)
+            ),
+            (
+                (0,1,0,0),
+                (0,1,1,0),
+                (0,0,1,0),
+                (0,0,0,0)
+            ),
+            (
+                (0,0,0,0),
+                (0,1,1,0),
+                (1,1,0,0),
+                (0,0,0,0)
+            ),
+            (
+                (1,0,0,0),
+                (1,1,0,0),
+                (0,1,0,0),
+                (0,0,0,0)
+            )
+        ),
+        (
+            (
+                (0,0,0,0),
+                (0,1,1,0),
+                (0,1,1,0),
+                (0,0,0,0)
+            ),
+            (
+                (0,0,0,0),
+                (0,1,1,0),
+                (0,1,1,0),
+                (0,0,0,0)
+            ),
+            (
+                (0,0,0,0),
+                (0,1,1,0),
+                (0,1,1,0),
+                (0,0,0,0)
+            ),
+            (
+                (0,0,0,0),
+                (0,1,1,0),
+                (0,1,1,0),
+                (0,0,0,0)
+            )
+        ),
+        (
+            (
+                (1,0,0,0),
+                (1,1,1,0),
+                (0,0,0,0),
+                (0,0,0,0)
+            ),
+            (
+                (0,1,1,0),
+                (0,1,0,0),
+                (0,1,0,0),
+                (0,0,0,0)
+            ),
+            (
+                (0,0,0,0),
+                (1,1,1,0),
+                (0,0,1,0),
+                (0,0,0,0)
+            ),
+            (
+                (0,1,0,0),
+                (0,1,0,0),
+                (1,1,0,0),
+                (0,0,0,0)
+            )
+        ),
+        (
+            (
+                (0,0,1,0),
+                (1,1,1,0),
+                (0,0,0,0),
+                (0,0,0,0)
+            ),
+            (
+                (0,1,0,0),
+                (0,1,0,0),
+                (0,1,1,0),
+                (0,0,0,0)
+            ),
+            (
+                (0,0,0,0),
+                (1,1,1,0),
+                (1,0,0,0),
+                (0,0,0,0)
+            ),
+            (
+                (1,1,0,0),
+                (0,1,0,0),
+                (0,1,0,0),
+                (0,0,0,0)
+            )
+        ),
+        (
+            (
+                (0,1,0,0),
+                (1,1,1,0),
+                (0,0,0,0),
+                (0,0,0,0)
+            ),
+            (
+                (0,1,0,0),
+                (0,1,1,0),
+                (0,1,0,0),
+                (0,0,0,0)
+            ),
+            (
+                (0,0,0,0),
+                (1,1,1,0),
+                (0,1,0,0),
+                (0,0,0,0)
+            ),
+            (
+                (0,1,0,0),
+                (1,1,0,0),
+                (0,1,0,0),
+                (0,0,0,0)
+            )
         )
     ]
+#         (
+#             (
+#                 (0,0,0,0),
+#                 (0,0,0,0),
+#                 (0,0,0,0),
+#                 (0,0,0,0)
+#             ),
+#             (
+#                 (0,0,0,0),
+#                 (0,0,0,0),
+#                 (0,0,0,0),
+#                 (0,0,0,0)
+#             ),
+#             (
+#                 (0,0,0,0),
+#                 (0,0,0,0),
+#                 (0,0,0,0),
+#                 (0,0,0,0)
+#             ),
+#             (
+#                 (0,0,0,0),
+#                 (0,0,0,0),
+#                 (0,0,0,0),
+#                 (0,0,0,0)
+#             )
+#         ),
     def __init__(self):
         self.map = [[0 for i in range(10)] for i in range(20)]
         self.block = [1, 0] # 블럭 종류 / 블럭 돌림 / 블럭 높이 / 블럭 위치
-        self.block_x = 5
-        self.block_y = 4
+        self.block_x = -4
+        self.block_y = 3
     def nextTick(self):
-        self.block_x += 1
+        is_overlap = False
+        map_c = self.copyMap()
+        if Main.block[0] != -1:
+            for i in range(4):
+                for j in range(4):
+                    x = i + self.block_x + 1
+                    y = j + self.block_y
+                    if x < 0:
+                        continue
+                    if y < 0 or y >=10:
+                        continue
+                    if Main.block[self.block[0]][self.block[1]][i][j] == 0:
+                        continue
+                    if x >= 20:
+                        is_overlap = True
+                        break
+                    if self.map[x][y] != 0:
+                        is_overlap = True
+                        break
+
+        if is_overlap == False:
+            self.block_x += 1
+        # 움직이는 블럭 초기화 / self.map 에다가 박아 넣음
+        else:
+            self.map = self.getMap()
+            self.block = [random.randint(1, Main.block_kind-1), 0]
+            self.block_x = -4
+            self.block_y = 3
         
     def turnBlock(self, dir): # dir 은 r 또는 l 만 받음
         if(dir == "r"):
             self.block[1] = (self.block[1] + 1) % 4
         if(dir == "l"):
             self.block[1] = (self.block[1] - 1) % 4
+    def moveBlock(self, dir):
+        if(dir == 'r'):
+            self.block_y += 1
+        if(dir == 'l'):
+            self.block_y -= 1
     def copyMap(self):
         map = []
         for i in self.map:
@@ -73,7 +264,6 @@ class Main:
         return map
     def getMap(self):
         map_c = self.copyMap()
-        count = 0
         if Main.block[0] != -1:
             for i in range(4):
                 for j in range(4):
@@ -83,8 +273,9 @@ class Main:
                         continue
                     if y < 0 or y >=10:
                         continue
+                    if Main.block[self.block[0]][self.block[1]][i][j] == 0:
+                        continue
                     map_c[x][y] = Main.block[self.block[0]][self.block[1]][i][j]
-                    count += 1
         return map_c
 
 def print_map(map):
@@ -99,7 +290,14 @@ def print_map(map):
 main = Main()
 
 if __name__ == "__main__":
-    print_map(main.getMap())
-    main.nextTick()
-    print("")
-    print_map(main.getMap())
+    for i in range(0, 1000):
+        if i % 3 == 0:
+            main.moveBlock('r')
+        if i % 5 == 0:
+            main.moveBlock('l')
+        time.sleep(0.1)
+        print_map(main.getMap())
+        main.nextTick()
+        print(i)
+
+
